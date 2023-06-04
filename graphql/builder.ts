d1f09a6877832eb699a55d75a729084dce2d9b84
Samith Fernando <samith.fernando@amusedgroup.com>
2023-06-04 09:57:33 +0530

    type correction

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -1,13 +1,13 @@
 // graphql/builder.ts
 import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from "@pothos/plugin-prisma";
-import type PrismaTypes from "@pothos/plugin-prisma/generated";
+// import type PrismaTypes from "@pothos/plugin-prisma/generated";
 import prisma from "../lib/prisma";
 import RelayPlugin from "@pothos/plugin-relay";
 import { createContext } from "./context";
 
 export const builder = new SchemaBuilder<{
-  PrismaTypes: PrismaTypes;
+  PrismaTypes: any;
   Context: ReturnType<typeof createContext>;
 }>({
   plugins: [PrismaPlugin, RelayPlugin],
